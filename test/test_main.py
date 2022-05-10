3e45a33f2ee0fd4eec99063a267ec6b14d059016
Sean Overton <sean14o8.so@gmail.com>
2022-05-10 23:00:43 +1000

    Introduced a test exmaple

---
+++ test/test_main.py
@@ -0,0 +1,15 @@
+"""
+Example test
+"""
+def inc(x):
+    """
+    Returns the argument incremented by one
+    """
+    return x + 1
+
+
+def test_answer():
+    """
+    Asserts example test
+    """
+    assert inc(4) == 5
