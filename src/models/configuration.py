4060b58cbeecd49f7c4d3e3b80964ff92cafcbda
Santi2468 <sss985@uowmail.edu.au>
2022-05-12 12:44:49 +1000

    added extra function to config file

--- src/models/configuration.py
+++ src/models/configuration.py
@@ -11,17 +11,20 @@ def create_hidden_dir():
     path = Path.home().joinpath('Secret')
     # check if path actually exists
     if not os.path.exists(path):
+        print(path)
         # creates new directory
         os.mkdir(path)
         # Sets hidden attribute to hide (+h hide -h reveal)
-        os.system("attrib +h " + str(path))
+        #os.system("attrib +h " + str(path))
+    return path
+
 
 # Singleton allows you to create only one instance of a class through the lifetime of a program
 # (metaclass=Singleton):will add later as you need to install package that isn't included in requirements
 class Configuration():
 
-    def __init__(self, db_file ='dicom.db'):
-        create_hidden_dir()
+    def __init__(self, db_path ='', db_file ='.dicom.db'):
+        self.db_path = create_hidden_dir().joinpath(db_file)
         self.set_up_db()
 
     def set_up_db(self):
@@ -29,7 +32,7 @@ class Configuration():
         Create database within the hidden directory
         """
         # Connection object to represent database
-        conn = sqlite3.connect(Path.home().joinpath('Secret'))# either pass file or create an in memory database
+        conn = sqlite3.connect(self.db_path)# either pass file or create an in memory database
         # creates database if it doesn't already exist
         conn.execute("""
                 CREATE TABLE IF NOT EXISTS CONFIGURATION (
@@ -45,7 +48,7 @@ class Configuration():
         Retrieves default directory from database
         """
         # Connection object to represent database
-        conn = sqlite3.connect(Path.home().joinpath('Secret'))
+        conn = sqlite3.connect(self.db_path)
         # creates cursor
         cursor = conn.cursor()
         # Gets default directory
@@ -55,8 +58,27 @@ class Configuration():
         conn.close()
         return path
 
-    # create update path
-
+    def update_default_dir(self, new_dir):
+        """
+        Adds/updates default directory
+        """
+        # Connection object to represent database
+        conn = sqlite3.connect(self.db_path)
+        # creates cursor
+        cursor = conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM CONFIGURATION;")
+        # check if file exists
+        if cursor.fetchone()[0]==0:
+            # if no files exist than it will insert the default path
+            conn.execute("""INSERT INTO configuration (id, default_dir) 
+                                VALUES (1, "%s");""" % new_dir)
+        else:
+            # if files exist than it will update the default path
+            conn.execute("""UPDATE CONFIGURATION
+                            SET default_dir = "%s"
+                            WHERE id = 1;""" % new_dir)
+        conn.commit()
+        conn.close()
 
 
 
