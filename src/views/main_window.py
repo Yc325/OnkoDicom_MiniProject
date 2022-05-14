488914a758f207a8927d55416bd11343dba38661
Sean Overton <sean14o8.so@gmail.com>
2022-05-14 14:10:12 +1000

    Minor comments/changes

--- src/views/main_window.py
+++ src/views/main_window.py
@@ -81,13 +81,12 @@ class MainView(QMainWindow):
         self.files_table.cellClicked.connect(self.select_image_file)
 
         # listen for model event signals
-        # self._model.amount_changed.connect(self.on_amount_changed)
         # listening for change in selected directory
         self._model.selected_dicom_directory_changed.connect(
             self.on_selected_dicom_directory_changed
         )
 
-        # checks for default directory
+        # checks for default directory preference in config db
         self._main_controller.check_preference()
 
     def on_selected_dicom_directory_changed(self, path):
@@ -102,27 +101,23 @@ class MainView(QMainWindow):
             path)
 
         for absolute_path in files:
-            # change this stat cmd as it produces a stats file
-            current_image_file_size = os.stat(absolute_path).st_size
+            current_image_file_size = os.path.getsize(absolute_path)
 
             file_name = absolute_path.split("/")[-1]
 
             file_name_item = QTableWidgetItem(file_name)
 
-            # fileNameItem.setFlags(fileNameItem.flags() ^ Qt.ItemIsEditable)
+            kb_files_size = int((current_image_file_size + 1023) / 1024)
             size_item = QTableWidgetItem(
-                f"{(int((current_image_file_size + 1023) / 1024))} KB")
-            # sizeItem.setTextAlignment(QtCore.Qt.AlignVCenter
-            # *| QtCore.Qt.AlignRight)
-            # sizeItem.setFlags(sizeItem.flags() ^ QtCore.Qt.ItemIsEditable)
+                f"{kb_files_size} KB")
 
             row = self.files_table.rowCount()
             self.files_table.insertRow(row)
             self.files_table.setItem(row, 0, file_name_item)
             self.files_table.setItem(row, 1, size_item)
 
-        # self.files_found_label.setText("%d file(s) found
-        # (Double click on a file to open it)" % len(files))
+        files_found_str = f"{len(files)} file(s) found (Click on a file to open it)"
+        self.files_found_label.setText(files_found_str)
 
     def select_image_file(self, row, column):
         """
--- src/views/popup_for_default_directory.py
+++ src/views/popup_for_default_directory.py
@@ -19,7 +19,6 @@ class Popup(QDialog):
         super().__init__()
 
         self._main_controller = main_controller
-        # super().__init__(parent)
         self.resize(500, 250)
 
         self.directory_label = QLabel("Directory:", self)
@@ -44,6 +43,7 @@ class Popup(QDialog):
                                                      QDir.currentPath())
         # updates database with new directory
         self._main_controller.get_config().update_default_dir(directory)
-        # updates parent window with new directory
+        
+        # asks the main controller to recheck db for directoy
         self._main_controller.check_preference()
         self.close()
